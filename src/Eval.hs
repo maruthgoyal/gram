d59696e7154d04fa842d5c95a7532f569024c75d
Maruth Goyal <maruthgoyal@gmail.com>
2019-05-04 11:01:39 -0500

    Added applySub, composeSub, formatting

--- src/Eval.hs
+++ src/Eval.hs
@@ -7,19 +7,24 @@ import qualified Data.Set as Set
 type Sub = (String, Expr)
 type Substitution = [Sub]
 
+-- Helper function to find free vars.
 fv' :: Expr -> Set.Set String -> Set.Set String
 fv' (Var x) s         = Set.insert x s
 fv' (App e1 e2) s     = Set.union (fv' e1 s) (fv' e2 s)
 fv' (Lam name body) s = Set.delete name (fv' body s)
 
+-- Finds the free variables in a given Expression
 fv :: Expr -> Set.Set String
 fv e = fv' e Set.empty
 
+-- Finds a new name which is not contained in the given set
 rename :: String -> Set.Set String -> String
 rename s set | Set.notMember s set = s
              | otherwise = rename (s ++ "'") set
 
+-- Application of one unit substitution to an Expression
 applyOneSub :: Sub -> Expr -> Expr
+applyOneSub _ (Lit l) = (Lit l)
 applyOneSub (x, y) (Var name) | x == name = y
                               | otherwise = (Var name)
 
@@ -28,8 +33,16 @@ applyOneSub s (App e1 e2) = (App (applyOneSub s e1) (applyOneSub s e2))
 applyOneSub (x, t') (Lam y t) | x == y = (Lam y t)
                               | otherwise =
                                       let
-                                      new_name = rename y (Set.union (fv t) (fv t'))
-                                      renamed_body = applyOneSub (y, (Var new_name)) t
-                                      final_body = applyOneSub (x, t') renamed_body
+                                              new_name = rename y (Set.union (fv t) (fv t'))
+                                              renamed_body = applyOneSub (y, (Var new_name)) t
+                                              final_body = applyOneSub (x, t') renamed_body
                                       in
-                                      (Lam new_name final_body)
+                                              (Lam new_name final_body)
+
+applySub :: Substitution -> Expr -> Expr
+applySub [] e     = e
+applySub (x:xs) e = applySub xs (applyOneSub x e)
+
+-- Composes 2 substitutions s1 and s2. Applies s2 to the range of s1.
+composeSub :: Substitution -> Substitution -> Substitution
+composeSub s1 s2 = [(s, (applySub s2 e)) | (s, e) <- s1]
--- src/Parser.hs
+++ src/Parser.hs
@@ -17,12 +17,15 @@ lexer = Tok.makeTokenParser style
                               Tok.reservedNames = names,
                               Tok.commentLine = "#"}
 
+-- Parses a single identifier
 identifier :: Parser String
 identifier = Tok.identifier lexer
 
+-- Parses a single reserved operation
 reservedOp :: String -> Parser ()
 reservedOp = Tok.reservedOp lexer
 
+-- Parses a Lambda Abstraction (Function)
 lambda :: Parser Expr
 lambda = do
         reservedOp "\\"
@@ -31,30 +34,36 @@ lambda = do
         body <- expr
         return $ foldr Lam body arguments
 
+-- Parses a single natural number
 number :: Parser Expr
 number = do
         n <- Tok.natural lexer
         return (Lit (LInt (fromIntegral n)))
 
+-- Parses an identifier, and creates a variable
 variable :: Parser Expr
 variable = do
         name <- identifier
         return (Var name)
 
+-- Finds a pair of parentheses
 parens :: Parser a -> Parser a
 parens = Tok.parens lexer
 
+-- Parses one term
 term :: Parser Expr
 term = parens expr
         <|> number
         <|> variable
         <|> lambda
 
+-- Parses one expression
 expr :: Parser Expr
 expr = do
         exprs <- many1 term
         return $ foldl1 App exprs
 
+-- Skips whitespace, applies the parser, finds the end-of-file
 contents :: Parser a -> Parser a
 contents p = do
         Tok.whiteSpace lexer
